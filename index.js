0862082e4ee707ee817fd0283e83b32f6d1de6c4
EzidZZe <dorofeevkirill2248@gmail.com>
2024-09-09 15:17:01 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+const data = []
+
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send(data)
+})
+app.post('/', (req, res) => {
+    data[data.length] = {
+        id: subjects.length,
+        tutor: req.body.tutor,
+        name: req.body.name,
+        classes: req.body.classes,
+        date: req.body.date
+    }
+})
+
+app.listen(4000, () => console.log('Server started!'))
